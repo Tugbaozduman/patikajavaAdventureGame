21aa351c1153720f095fc1fe4757164b5fd1a267
Tugbaozduman <77573771+Tugbaozduman@users.noreply.github.com>
2022-05-11 12:42:10 +0300

    Create samurai.java

---
+++ samurai.java
@@ -0,0 +1,10 @@
+package MaceraOyunu.CharClasses;
+
+import MaceraOyunu.GameChar;
+
+public class Samurai extends GameChar {
+    public Samurai() {
+        super(1,"Samuray",5, 21, 15);
+    }
+}
+© 2022 GitHub, Inc.
